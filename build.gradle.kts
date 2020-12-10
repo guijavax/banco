b13ce13d91c40f0027528826d5471788a8a23ad0
Guilheme <guijavax>
2020-12-09 23:11:30 -0300

    adicao de dependencia

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("com.google.code.gson:gson:2.8.5")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
